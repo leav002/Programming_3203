340cef2d48958d6cc77e495d3d9e240b67ed81d7
H4A <hordesiosjinu@gmail.com>
2026-03-31 09:45:10 +0900

    CounterApp reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -14,6 +14,8 @@ function CounterApp() {
     <button onClick={()=> setCount((count) => count + 1)}>+</button>
     <button onClick={()=> setCount((count) => count + 2)}>+2</button>
     <button onClick={()=> setCount((count) => count - 1)}>-</button>
+    <button onClick={()=> setCount(0)}>reset</button>
+
 
 
       {/* <button
